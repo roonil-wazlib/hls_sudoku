07372d84874051bdc317530b5c7909cf38c5962e
roonil-wazlib <elfhogan@gmail.com>
2020-04-04 19:06:04 +1300

    Added Sudoku generation files

---
+++ generate_sudoku.py
@@ -0,0 +1,156 @@
+import random
+from sudoku_classes import *
+
+
+def generate_ordered_2d_board():
+    """ Generate an unshuffled 2d sudoku board """
+    board = []
+    
+    for i in range(3):
+        for j in range(3):
+            board.append([((x+i) + 3*j) % 9 + 1 for x in range(9)])
+            
+    return board
+
+
+def generate_shuffled_2d_board():
+    """ Generate a 'random' 2D Sudoku board """
+    
+    ordered = generate_ordered_2d_board()
+    columns_shuffled = shuffle_columns(ordered)
+    # rows_shuffled = shuffle_rows(columns_shuffled)
+    relabeled = relabel_values(columns_shuffled)
+
+    return relabeled
+    
+    
+    
+def shuffle_columns(board):
+    """ Shuffle the columns of a 2D board"""
+    shuffled = []
+    column_group_order = [0,1,2]
+    random.shuffle(column_group_order)
+    for i in column_group_order:
+        #shuffle columns within each of the 3 column groups
+        column_order = [0,1,2]
+        random.shuffle([0,1,2])
+        for j in column_order:
+            shuffled.append(board[3*i + j])
+            
+    return shuffled
+    
+    
+def shuffle_rows(board):
+    """ Shuffle the rows of a 2D board"""
+    #returns a 90* rotation but who cares, we're shuffling anyway
+    
+    #convert rows to columns:
+    board = rows_to_columns(board)
+
+    #now shuffle as columns
+    return shuffle_columns(board)
+    
+    
+def relabel_values(board):
+    """makes the board look more random but actually just produces the same board (isomorphic by relabelling)"""
+    values = [1,2,3,4,5,6,7,8,9]
+    random.shuffle(values)
+    
+    output = []
+    
+    for x in board:
+        col = []
+        for y in x:
+            col.append(values[y-1])
+        output.append(col)
+        
+    return output
+    
+    
+def rows_to_columns(board):
+    """Return a 90* rotation of the board in the same list representation"""
+    
+    output = []
+    for i in range(9):
+        col = []
+        for j in range(9):
+            col.append(board[j][i])
+        output.append(col)
+        
+    return output
+    
+    
+def shuffle_cube(cube):
+    """shuffle the columns around within the 3D implementation"""
+    shuffled = []
+    column_group_order = [0,1,2]
+    random.shuffle(column_group_order)
+    for i in column_group_order:
+        #shuffle columns within each of the 3 column groups
+        column_order = [0,1,2]
+        random.shuffle([0,1,2])
+        for j in column_order:
+            shuffled.append(cube[3*i + j])
+
+    return shuffled    
+    
+    
+def generate_3d_board():
+    """generate 3D cube from 2D board"""
+    layer = generate_shuffled_2d_board()
+    cube = []
+    for i in range(len(layer)):
+        new_layer = []
+        for column in layer:
+            new_column = []
+            #this nested mess is to ensure that none of the sub 3x3 squares violates sudoku rules from any x y or z perspective
+            #(also the Latin Square rules but the subsquares are trickier and the cause of more mess)
+            for j in range(3):
+                for k in range(3):
+                    #lot of 3 = (i+j) % 3
+                    #index within lot = (i + k + (i//3)) % 3 
+                    new_column.append(column[3*((i + j) % 3) + (i + k + (i // 3)) % 3])
+            new_layer.append(new_column)
+        cube.append(new_layer)
+        
+    return shuffle_cube(cube)
+
+
+def generate_unshuffled_3d_board():
+    """for purposes of bruteforcing the hardest uniquely solvable game"""
+    layer = generate_ordered_2d_board()
+    cube = []
+    for i in range(len(layer)):
+        new_layer = []
+        for column in layer:
+            new_column = []
+            #this nested mess is to ensure that none of the sub 3x3 squares violates sudoku rules from any x y or z perspective
+            #(also the Latin Square rules but the subsquares are trickier and the cause of more mess)
+            for j in range(3):
+                for k in range(3):
+                    #lot of 3 = (i+j) % 3
+                    #index within lot = (i + k + (i//3)) % 3 
+                    new_column.append(column[3*((i + j) % 3) + (i + k + (i // 3)) % 3])
+            new_layer.append(new_column)
+        cube.append(new_layer)
+        
+    return cube
+
+
+def convert_to_game(cube):
+    """ convert a valid Sudoku cube to a solvable game """
+    game_cube = cube[:]
+    count = 0
+    #remove 500 elements from the cube at random
+    while count < 500:
+        x = random.randrange(0,9,1)
+        y = random.randrange(0,9,1)
+        z = random.randrange(0,9,1)
+        if game_cube[x][y][z] != "":
+            count += 1
+            game_cube[x][y][z] = ""
+        
+    return game_cube
+
+
+
---
+++ sudoku_classes.py
@@ -0,0 +1,152 @@
+class Sudoku:
+    
+    def __init__(self, ls, solution=True):
+        self.board = ls
+        if solution:
+            if not self.is_correct():
+                for x in self.board:
+                    print(x)
+                raise Exception("This is an invalid Sudoku board")
+    
+    
+    def insert_value(self, val, x, y):
+        """Insert value at coordinates (x), (y)"""
+        self.board[x][y] = val
+
+    
+    def is_correct(self):
+        """return True if current board satisfies Latin Square and subsquare properties of Sudoku"""
+        
+        if self._x_correct() and self._y_correct() and self._sub_squares_correct():
+            return True
+        else:
+            return False
+        
+    
+    def _y_correct(self):
+        """check columns are of valid latin square"""
+        
+        for i in range(len(self.board)):
+            items = set()
+            for element in self.board[i] :
+                if element in items and element != "":
+                    return False
+                else:
+                    items.add(element)
+                    
+        return True
+                
+                
+    def _x_correct(self):
+        """check rows are of valid latin square"""
+        for i in range(len(self.board)):
+            items = set()
+            for column in self.board:
+                if column[i] in items and column[i] != "":
+                    return False
+                else:
+                    items.add(column[i])
+                    
+        return True
+                    
+    
+    def _sub_squares_correct(self):
+        """check if each subsquare is a valid sudoku subsquare"""
+        #loop x,y coordinates of subsquares
+        for x in range(int(len(self.board)/3)):
+            for y in range(int(len(self.board)/3)):
+                items = set()
+                
+                #loop j,k coordinates of elements in subsquare
+                for i in range(int(len(self.board) / 3)):
+                    for j in range(int(len(self.board) / 3)):
+                        if self.board[3*x + i][3*y + j] in items and self.board[3*x + i][3*y + j] != "":
+                            return False
+                        else:
+                            items.add(self.board[3*x + i][3*y + j])       
+        return True
+    
+    
+    def __len__(self):
+        return len(self.board)
+        
+    def __getitem__(self, index):
+        return self.board[index]
+    
+    def __repr__(self):
+        output = ""
+        for item in self.board:
+            output += " ".join([str(x) for x in item])
+            output += "\n"
+        return output
+                
+        
+        
+
+class Sudoku3D(Sudoku):
+    
+    def __init__(self, ls, solution=True):
+        self.solution = solution
+        self.x_elements = [Sudoku(x, self.solution) for x in ls] # list representation in column form as viewed from x axis
+        self.y_elements = self.get_y_view()
+        self.z_elements = self.get_z_view()
+    
+    
+    def insert_value(self, val, x, y, z):
+        """insert value at coordinates x, y, z"""
+        self.x_elements[z].insert_value(val, x, y)
+        self.y_elements[x].insert_value(val, z, y)
+        #self.z_elements[z].insert_value(val, y, x) - figure this out
+        
+        self.z_elements = self.get_z_view()
+        
+        
+    def check(self, solution):
+        """return a list of values that player has entered incorrectly"""
+        incorrect = []
+        for x in range(len(self)):
+            for y in range(len(self)):
+                for z in range(len(self)):
+                    if self.x_elements[z][y][x] != solution.x_elements[z][y][x] and self.x_elements[z][y][x] != "":
+                        incorrect.append((x,y,z))
+                        
+        return incorrect
+    
+        
+    def get_x_view(self):
+        return self.elements
+    
+    
+    def get_y_view(self):
+        """
+        'rotate' Sudoku cube to get view from y perspective
+        """
+        # nth list of all 2nd tier lists in x_view forms columns of nth 'layer' in y_view
+        
+        y_view = []
+        for i in range(len(self.x_elements)): # works for arbitrary sized sudoku
+            y_view.append([x.board[i] for x in self.x_elements])
+            
+        y_view = [Sudoku(x, self.solution) for x in y_view]
+        return y_view
+    
+    
+    def get_z_view(self):
+        """
+        'rotate' Sudoku cube to get view from z perspective
+        """
+        
+        # we want nth element of all columns for all layers in x_view
+        
+        z_view = []
+        
+        for i in range(len(self.x_elements)): # works for arbitrary sized sudoku
+            z_view.append([[x[i] for x in y.board] for y in self.x_elements])
+            
+        z_view = [Sudoku(x, self.solution) for x in z_view]
+        return z_view
+    
+    
+    def __len__(self):
+        return len(self.x_elements) #doesn't matter which dimension - it's a cube
+            
